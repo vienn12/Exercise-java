b43614393c559b1e299cd0a0ed8d366080dc49e0
vienn12 <ledinhvien108@gmail.com>
2025-08-14 22:15:04 +0700

    vien

---
+++ src/exe21/exe21.java
@@ -0,0 +1,12 @@
+
+package exe21;
+
+
+public class exe21 {
+     public static void main(String[] args) {
+      int x=1;
+      int y=x+ ++x - x-- -x++ +x-x--;
+        System.out.println("x: "+x);
+        System.out.println("y: "+y);
+              }
+}
